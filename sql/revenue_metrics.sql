0962bec6b8bd0d84394aff37b21827c8f894d3a5
Andrew Yaw Afodoanyi <100138784+BoiDrew@users.noreply.github.com>
2026-01-08 19:39:11 +0100

    Refactor code structure for improved readability and maintainability

--- sql/revenue_metrics.sql
+++ sql/revenue_metrics.sql
@@ -61,3 +61,76 @@ FROM fact_revenue
 ORDER BY revenue_date
 LIMIT 10;
 
+# monthly MRR
+SELECT
+    revenue_date,
+    ROUND(SUM(amount), 2) AS mrr
+FROM fact_revenue
+GROUP BY revenue_date
+ORDER BY revenue_date;
+
+
+#Revenue Retention by Cohort 
+WITH cohort_revenue AS (
+    SELECT
+        DATE_FORMAT(dc.signup_date, '%Y-%m-01') AS cohort_month,
+        TIMESTAMPDIFF(
+            MONTH,
+            DATE_FORMAT(dc.signup_date, '%Y-%m-01'),
+            fr.revenue_date
+        ) AS months_since_signup,
+        SUM(fr.amount) AS cohort_revenue
+    FROM dim_customers dc
+    JOIN fact_revenue fr
+      ON dc.customer_id = fr.customer_id
+    GROUP BY cohort_month, months_since_signup
+),
+cohort_base AS (
+    SELECT
+        cohort_month,
+        cohort_revenue AS month0_revenue
+    FROM cohort_revenue
+    WHERE months_since_signup = 0
+)
+SELECT
+    cr.cohort_month,
+    cr.months_since_signup,
+    cr.cohort_revenue,
+    ROUND(
+        cr.cohort_revenue * 1.0 / cb.month0_revenue,
+        4
+    ) AS revenue_retention_rate
+FROM cohort_revenue cr
+JOIN cohort_base cb
+  ON cr.cohort_month = cb.cohort_month
+ORDER BY cr.cohort_month, cr.months_since_signup;
+
+
+# ARPU (Average Revenue Per User)
+SELECT
+    fr.revenue_date,
+    ROUND(
+        SUM(fr.amount) / COUNT(DISTINCT fr.customer_id),
+        2
+    ) AS arpu
+FROM fact_revenue fr
+GROUP BY fr.revenue_date
+ORDER BY fr.revenue_date;
+
+# Avg revenue per active customer
+SELECT
+    ROUND(AVG(amount), 2) AS avg_monthly_revenue_per_customer
+FROM fact_revenue;
+
+
+# Avg active lifespan (months)
+SELECT
+    ROUND(AVG(active_months), 2) AS avg_active_months
+FROM (
+    SELECT
+        customer_id,
+        COUNT(DISTINCT revenue_date) AS active_months
+    FROM fact_revenue
+    GROUP BY customer_id
+) t;
+
